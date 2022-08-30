e6bb30c122e6fd5260002ce9cc5e376724f9c486
Mariano <marianonunezma@gmail.com>
2022-08-30 11:58:43 -0300

    Vercel config

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.use((error, request, response, next) => {
   next(error);
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
