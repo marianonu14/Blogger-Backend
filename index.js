6e4b9e07497f53e106207b190485aedb01b775fc
Mariano <marianonunezma@gmail.com>
2022-08-30 13:46:12 -0300

    Fixing config

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static('build'));
 
 app.get('/', (request, response) => {
-  response.sendFile('index.html');
+  response.send('Algo');
 });
 
 app.get('/api/notes', (request, response) => {
